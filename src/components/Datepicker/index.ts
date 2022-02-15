004b5efc6b2e8bcecf96b67c4ea99d352a202c7c
Jason Shimkoski <jshimkoski@me.com>
2022-02-15 16:06:32 -0500

    fix datepicker index.ts

--- src/components/Datepicker/index.ts
+++ src/components/Datepicker/index.ts
@@ -1,5 +1,5 @@
 import { App } from "vue";
-import Component from "./Calendar.vue"
+import Component from "./Datepicker.vue"
 
 Component.install = (Vue: App) => {
   Vue.component(Component.name, Component);
